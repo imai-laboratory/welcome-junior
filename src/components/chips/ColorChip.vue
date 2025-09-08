c37a53c94ef0f821712e96b50e473fe21bc90d26
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2025-09-09 02:22:15 +0900

    fix: Fix base url

--- src/components/chips/ColorChip.vue
+++ src/components/chips/ColorChip.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
-
 type Props = {
 	/** 表示するテキスト */
 	label: string;
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		vue(),
-		tailwindcss(),
-		viteStaticCopy({
-			targets: [{ src: 'public/past_images/*', dest: './images' }],
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+	// .env ファイル or process.env から読み込み
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		base: env.VITE_BASE_PATH,
+		plugins: [
+			vue(),
+			tailwindcss(),
+			viteStaticCopy({
+				targets: [{ src: 'public/past_images/*', dest: './images' }],
+			}),
+		],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
 		},
-	},
-	server: {
-		port: 3000,
-	},
+		server: {
+			port: 3000,
+		},
+	};
 });
